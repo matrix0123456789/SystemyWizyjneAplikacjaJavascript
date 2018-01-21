ff5a5c698e495e2f4b0e58fb28f095319dba81dd
matrix0123456789 <Matrix0123456789@tlen.pl>
2018-01-21 12:03:24 +0100

    rozwój

--- js/generate.js
+++ js/generate.js
@@ -2,6 +2,10 @@ buttons.push({name: '20 figures', fun: (inputBitmap,context) => {
       generateFigures(20,context);
     }});
 function generateFigures(count, context){
+
+context.fillStyle = 'black';
+
+context.fillRect(0,0,255,255);
       var figures=[];
       while(figures.length<count){
 
@@ -11,7 +15,10 @@ function generateFigures(count, context){
                         type:Math.floor(Math.random()*6), 
                         points:[]};
             if(figure.type<3){
-                  
+                  context.beginPath();
+      context.arc(figure.x, figure.y, figure.r, 0, 2 * Math.PI, false);
+      context.fillStyle = 'black';
+      context.fill();
             }else{
                   var alfaChange=360/figure.type;
                   if(figure.type%2==0){
@@ -20,10 +27,18 @@ function generateFigures(count, context){
                         alfa=-90;
                   }
                   for(var j=0;j<figure.type;j++){
-                  figure.points.push({x:figure.x+Math.cos(alfa*Math.Pi/180),y:figure.x+Math.cos(alfa*Math.Pi/180)});
+                  figure.points.push({x:figure.x+Math.cos(alfa*Math.PI/180),y:figure.x+Math.cos(alfa*Math.PI/180)});
+                        alfa+=alfaChange;
                   }
-            }
-            figures.push([]);
+           
+context.fillStyle = 'black';
+context.beginPath();
+for(var point of figure.points){
+context.moveTo(point.x, point.y);}
+context.closePath();
+context.fill();
+                   }
+            figures.push(figure);
       }
 
 }
