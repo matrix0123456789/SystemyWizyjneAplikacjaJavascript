7f638ab709f5a6e0edbeb926bcc42080aa9c0a51
matrix0123456789 <Matrix0123456789@tlen.pl>
2018-01-21 11:47:30 +0100

    r

--- js/generate.js
+++ js/generate.js
@@ -6,12 +6,17 @@ var figures=[];
       var i=0;
       while(i<count){
 
-var figure={x:Math.floor(Math.random()*256),y:Math.floor(Math.random()*256),r:12+Math.floor(Math.random()*10),type:Math.floor(Math.random()*6), points=[]};
+            var figure={x:Math.floor(Math.random()*256),
+                        y:Math.floor(Math.random()*256),
+                        r:12+Math.floor(Math.random()*10),
+                        type:Math.floor(Math.random()*6), 
+                        points:[]};
             if(figure.type<3){
+                  
             }else{
                   var alfaChange=360/figure.type;
                   if(figure.type%2==0){
-                   var alfa=-90+  alfaChange/2;   
+                        var alfa=-90+  alfaChange/2;   
                   }else{
                         alfa=-90;
                   }
